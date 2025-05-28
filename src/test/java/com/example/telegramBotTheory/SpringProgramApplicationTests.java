f34ec5c0f9dab5a7dfb3acddf95c1e0b769565d9
Maxim <Maximka_lord@list.ru>
2025-05-28 18:29:13 +1000

    Initial commit

---
+++ src/test/java/com/example/telegramBotTheory/SpringProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegramBotTheory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
